88b17a06d2f2b4c0a4417ad8020c3ba011dd5992
beckheng <beckheng@gmail.com>
2014-05-16 16:31:36 +0800

    添加注释

--- util/util.h
+++ util/util.h
@@ -1,6 +1,5 @@
 //
 //  util.h
-//  FlyingTigerServer
 //
 //  Created by beckheng on 14-4-24.
 //  Copyright (c) 2014年 beckheng. All rights reserved.
@@ -12,13 +11,19 @@
 #include <iostream>
 #include <vector>
 
-double getMilliSecond();
-double getMicroSecond();
+double getMilliSecond(); // 取毫秒
+double getMicroSecond(); // 取微秒
 
+// 字符串替换
 void stringReplace(std::string &s, const std::string &search, const std::string &replace);
+
+// 字符串分割
 std::vector<std::string> split(const std::string &s, const std::string &sep);
 
+// 格式化日期显示，以当前时间格式化
 std::string formatDateTime(const char*format);
+
+// 格式化日期显示，指定时间格式化
 std::string formatDateTime(const struct tm *t, const char *format);
 
-#endif /* defined(__FlyingTigerServer__util__) */
+#endif
